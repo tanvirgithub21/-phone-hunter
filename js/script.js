909f0f0c559555cb73022dddfaa60646ca7934a3
Tanvir Ahmed <tanvir.be.global@gmail.com>
2022-03-01 23:10:00 +0600

    added all card style and funshion

--- js/script.js
+++ js/script.js
@@ -6,33 +6,38 @@ const allPhones = () => {
 
     fetch(playerUrl)
     .then(res => res.json())
-    .then(data => displayPhoneUI(data.data))
+    .then(data => displayPhoneUI(data.data, data.status))
 };
 
 // display UI all phones 
-const displayPhoneUI = phones => {
+const displayPhoneUI = (phones, availableData) => {
     const phoneContainer = document.getElementById("showPhoneCard");
     phoneContainer.innerText = "";
-    for(phone of phones){
-        const addNewCard = document.createElement("div");
-        addNewCard.classList.add('card', 'col-lg-4', 'my-3');
 
-        addNewCard.innerHTML = `
-            <img src=${phone.image} class="card-img-top" id="phoneImg"  alt="iphone">
-            <div class="card-body">
-                <h5 class="card-title PhoneName text-center">${phone.phone_name}</h5>
-                <p class="card-text PhoneBrand text-center">${phone.brand}</p>
-
-                <div class="cardBtnDiv">
-                    <button class="button" id="detailsBtn">Details</button>
+    if(availableData){
+        for(phone of phones){
+            const addNewCard = document.createElement("div");
+            addNewCard.classList.add('card', 'cardBox', 'col-lg-4', 'my-3');
+    
+            addNewCard.innerHTML = `
+                <img src=${phone.image} class="card-img-top" id="phoneImg"  alt="iphone">
+                <div class="card-body">
+                    <h5 class="card-title PhoneName text-center">${phone.phone_name}</h5>
+                    <p class="card-text PhoneBrand text-center">${phone.brand}</p>
+    
+                    <div class="cardBtnDiv">
+                        <button class="button" id="detailsBtn">Details</button>
+                    </div>
+    
                 </div>
-
-            </div>
-
-        `;
-
-        phoneContainer.appendChild(addNewCard);
-        // console.log(phone)
+            `;
+    
+            phoneContainer.appendChild(addNewCard);
+            // console.log(phone)
+        }
+    }
+    else{
+        const noDataFound = document.getElementById("noDataFound").style.display = "flex"
     }
 }
 
